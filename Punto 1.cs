bba1ed5cc96251e43dfda28c90821f87a58edecd
SadMinnnn <128757228+SadMinnnn@users.noreply.github.com>
2023-03-23 18:23:48 -0500

    Add files via upload

---
+++ Punto 1.cs
@@ -0,0 +1,175 @@
+﻿public interface IMonoplaza
+{
+    void Encender();
+    void Apagar();
+    void Detener();
+    void Mover();
+}
+
+public abstract class Monoplaza : IMonoplaza
+{
+    private bool encendido;
+    private bool movimiento;
+
+    public bool Marca { get; set; }
+
+    public Monoplaza()
+    {
+        encendido = false;
+        movimiento = false;
+    }
+
+    public void Encender()
+    {
+        if (!encendido)
+        {
+            encendido = true;
+            Console.WriteLine("El auto está encendido.");
+        }
+    }
+
+    public void Apagar()
+    {
+        if (encendido && !movimiento)
+        {
+            encendido = false;
+            Console.WriteLine("El auto está apagado.");
+        }
+    }
+
+    public void Detener()
+    {
+        if (encendido && movimiento)
+        {
+            movimiento = false;
+            Console.WriteLine("El auto está detenido.");
+        }
+    }
+
+    public void Mover()
+    {
+        if (encendido && !movimiento)
+        {
+            movimiento = true;
+            Console.WriteLine("El auto se está moviendo.");
+        }
+    }
+
+    public abstract int TiempoV();
+}
+
+
+public class McLaren : Monoplaza
+{
+    public override int TiempoV()
+    {
+        
+        Random rnd = new Random();
+        return rnd.Next(100000, 999999);
+    }
+}
+
+
+public class Ferrari : Monoplaza
+{
+    public override int TiempoV()
+    {
+        
+        Random rnd = new Random();
+        return rnd.Next(100000, 999999);
+    }
+}
+
+
+public class Redbull : Monoplaza
+{
+    public override int TiempoV()
+    {
+        
+        Random rnd = new Random();
+        return rnd.Next(100000, 999999);
+    }
+}
+
+
+public class Circuito
+{
+    private Monoplaza monoplaza;
+    private int vueltas;
+    private string nombre;
+
+    public Circuito(int vueltas, string nombre)
+    {
+        this.vueltas = vueltas;
+        this.nombre = nombre;
+    }
+
+    public void AggMonoplaza(Monoplaza monoplaza)
+    {
+        if (this.monoplaza == null)
+        {
+            this.monoplaza = monoplaza;
+            Console.WriteLine("Auto listo para empezar vuelta.");
+        }
+    }
+
+    public void SacarMonoplaza()
+    {
+        if (monoplaza != null)
+        {
+            monoplaza = null;
+            Console.WriteLine("Auto fuera de circuito.");
+        }
+    }
+
+    public void RealizarPrueba()
+    {
+        if (monoplaza != null)
+        {
+            Console.WriteLine("Realizando vuelta");
+
+            int mejorTiempo = int.MaxValue;
+
+            for (int i = 1; i <= vueltas; i++)
+            {
+                int tiempo = monoplaza.TiempoV();
+                Console.WriteLine("Vuelta " + i + ": " + tiempo + " ms.");
+
+                if (tiempo < mejorTiempo)
+                {
+                    mejorTiempo = tiempo;
+                }
+            }
+
+            Console.WriteLine("Tiempo del ganador: " + mejorTiempo + " ms.");
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        McLaren mclaren = new McLaren();
+        Ferrari ferrari = new Ferrari();
+        Redbull redbull = new Redbull();
+
+       
+        Circuito circuito = new Circuito(5,"Circuito A");
+
+        
+        circuito.AggMonoplaza(mclaren);
+        circuito.RealizarPrueba();
+        circuito.SacarMonoplaza();
+
+        circuito.AggMonoplaza(ferrari);
+        circuito.RealizarPrueba();
+        circuito.SacarMonoplaza();
+
+        circuito.AggMonoplaza(redbull);
+        circuito.RealizarPrueba();
+        circuito.SacarMonoplaza();
+
+        Console.ReadKey();
+    }
+}
---
+++ Punto 2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Punto2
+{
+   
+    public interface IPastel
+    {
+        string Nombre { get; set; }
+        int Tamaño { get; set; }
+        List<Ingrediente> Ingredientes { get; set; }
+
+        void AgregarIngrediente(Ingrediente ingrediente);
+        int CantidadIngredientes();
+        string ListarIngredientes();
+        decimal CalcularCosto();
+    }
+
+    
+    public class Ingrediente
+    {
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+
+        public Ingrediente(string nombre, int cantidad, decimal precio)
+        {
+            Nombre = nombre;
+            Cantidad = cantidad;
+            Precio = precio;
+        }
+    }
+
+    
+    public class Pastel : IPastel
+    {
+        public string Nombre { get; set; }
+        public int Tamaño { get; set; }
+        public List<Ingrediente> Ingredientes { get; set; }
+
+        public Pastel(string nombre, int tamaño)
+        {
+            Nombre = nombre;
+            Tamaño = tamaño;
+            Ingredientes = new List<Ingrediente>();
+        }
+
+        public void AgregarIngrediente(Ingrediente ingrediente)
+        {
+            Ingredientes.Add(ingrediente);
+        }
+
+        public int CantidadIngredientes()
+        {
+            return Ingredientes.Count;
+        }
+
+        public string ListarIngredientes()
+        {
+            string lista = "";
+            foreach (Ingrediente i in Ingredientes)
+            {
+                lista += i.Cantidad + " " + i.Nombre + " $" + i.Precio + "\n";
+            }
+            return lista;
+        }
+
+        public decimal CalcularCosto()
+        {
+            decimal costo = 0;
+            foreach (Ingrediente i in Ingredientes)
+            {
+                costo += i.Precio;
+            }
+            return costo;
+        }
+    }
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Pastel pastel = new Pastel("Pastel de chocolate", 10);
+            pastel.AgregarIngrediente(new Ingrediente("Harina", 2, 30));
+            pastel.AgregarIngrediente(new Ingrediente("Huevos", 4, 5));
+            pastel.AgregarIngrediente(new Ingrediente("Azúcar", 1, 15));
+            pastel.AgregarIngrediente(new Ingrediente("Chocolate", 3, 40));
+
+            
+            Console.WriteLine("El pastel " + pastel.Nombre + " tiene " + pastel.CantidadIngredientes() + " ingredientes:");
+            Console.WriteLine(pastel.ListarIngredientes());
+            Console.WriteLine("El costo del pastel es $" + pastel.CalcularCosto());
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
